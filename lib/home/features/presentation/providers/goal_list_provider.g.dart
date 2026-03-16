9a300b84cc5b830316c151f878dc9b9401be199d
Iliya Cherman <cherman9966@gmail.com>
2026-03-16 13:00:00 +0200

    feat:(progress logic) "added dynamic change streack and display of progress by day with control of skipping days"

---
+++ lib/home/features/presentation/providers/goal_list_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'goal_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$goalListHash() => r'579ca4b851f41193dc80d731515b38edbc9cdb6a';
+
+/// See also [GoalList].
+@ProviderFor(GoalList)
+final goalListProvider =
+    AutoDisposeAsyncNotifierProvider<GoalList, List<GoalEntity>>.internal(
+  GoalList.new,
+  name: r'goalListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$goalListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GoalList = AutoDisposeAsyncNotifier<List<GoalEntity>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/home/features/presentation/providers/repository_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$goalRepositoryHash() => r'ed5cccfd628c758a83c7a5d0e389188a9fff6a72';
+
+/// See also [goalRepository].
+@ProviderFor(goalRepository)
+final goalRepositoryProvider = Provider<GoalRepository>.internal(
+  goalRepository,
+  name: r'goalRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$goalRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GoalRepositoryRef = ProviderRef<GoalRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
